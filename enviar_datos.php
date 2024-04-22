814d60d897c7c063e561643a2979b4324ff2ca3f
Mario <marioandres.1996@icloud.com>
2024-04-21 21:18:49 -0500

    Formulario

--- enviar_datos.php
+++ enviar_datos.php
@@ -3,7 +3,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombre = $_POST["nombre"];
 
     // Ejecutar el script de Python con los datos del formulario como argumentos
-    $comando = escapeshellcmd('python3 app.py ' . escapeshellarg($nombre) . ' ' . escapeshellarg($lugar) . ' ' . escapeshellarg($tiempo));
+    $comando = escapeshellcmd('python3 app.py ' . escapeshellarg($nombre));
     
     // Ejecutar el comando y capturar la salida (opcional)
     $output = shell_exec($comando);
